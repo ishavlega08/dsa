330950c6eeff5128691fda3664285c569f400536
Ishav <ishavlega@gmail.com>
2023-09-15 15:17:42 +0530

    vectors

---
+++ Lecture44/vectors.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+
+    // declare
+    vector<int> arr;
+
+    // int ans = (sizeof(arr))/(sizeof(int));
+    // cout<<ans<<endl;
+
+    cout<<arr.size()<<endl;
+    cout<<arr.capacity()<<endl;
+
+    // insert
+    arr.push_back(5);
+    arr.push_back(7);
+
+    // print
+    for(int i=0; i<arr.size(); i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+
+    // remove/delete
+    arr.pop_back();
+
+    // print
+    for(int i=0; i<arr.size(); i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+
+    // runtime size
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+
+    vector<int> brr(n, -1);
+    cout<<"Size of b: "<<brr.size()<<endl;
+    cout<<"Capacity of b: "<<brr.capacity()<<endl;
+
+    for(int i=0; i<brr.size(); i++){
+        cout<<brr[i]<<" ";
+    }cout<<endl;
+
+    cout<<"Vector brr is empty or not: "<<brr.empty()<<endl;
+
+    // syntax is correct but showing error
+    // this is used very less
+    // cout<<"Printing crr: ";
+    // vector<int> crr{10,20,30,50};
+    // for(int i=0; i<crr.size(); i++){
+    //     cout<<crr[i]<<" ";
+    // }cout<<endl;
+
+ return 0;
+}
