e265f35e3035a738c4445d1ec5e520f3acf03690
Ishav <ishavlega@gmail.com>
2023-09-18 16:35:34 +0530

    homework

--- Lecture44/homework.cpp
+++ Lecture44/homework.cpp
@@ -45,6 +45,32 @@ void rotate1(int arr[], int n, int d){
     }
 }
 
+void findMajority(int arr[], int n){
+    int maxCount = 0;
+    int index=-1;
+    
+    for(int i=0; i<n; i++){
+        int count = 0;
+        for(int j=0; j<n; j++){
+            if(arr[i]==arr[j])
+                count++;
+        }
+
+        // update maxCount if count of current element is greater
+        if(count>maxCount){
+            maxCount = count;
+            index = i;
+        }
+    }
+
+    // if maxCount is greater than n/2 return the corresponding element
+    if(maxCount>n/2)
+        cout<<arr[index]<<endl;
+    else
+        cout<<"No majority element found"<<endl;
+
+}
+
 void printArr(int arr[], int size){
     for(int i=0; i<size; i++){
         cout<<arr[i]<<" ";
@@ -54,13 +80,21 @@ void printArr(int arr[], int size){
 int main(){
 
     // ARRAY LEFT ROTATION BY D POSITIONS
-    int arr[] = {1, 2, 3, 4, 5, 6, 7};
-    int N = sizeof(arr)/sizeof(int);
+    // int arr[] = {1, 2, 3, 4, 5, 6, 7};
+    // int N = sizeof(arr)/sizeof(int);
 
-    int d = 3;
+    // int d = 3;
 
-    rotate1(arr, N, d);
-    printArr(arr, N);
+    // rotate1(arr, N, d);
+    // printArr(arr, N);
+
+    // MAJORITY ELEMENT IN ARRAY
+    int arr[] = {1, 1, 2, 1, 3, 5, 1};
+
+    int n = sizeof(arr)/sizeof(int);
+    // cout<<n<<endl;
+
+    findMajority(arr, n);
 
  return 0;
 }
